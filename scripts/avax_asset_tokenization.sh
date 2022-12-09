39ed93f6321568854cba442306f1bef8545f9bdc
ryojiroakiyama <ryojiro25@gmail.com>
2022-12-09 17:08:47 +0900

    add avax test

---
+++ scripts/avax_asset_tokenization.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Clean existing test code.
+rm -rf $PATH_TO_SUBMISSION_REPO/contract/test
+
+# Copy original test code
+cp -r $PATH_TO_TEST_SOURCE/contract/test $PATH_TO_SUBMISSION_REPO/contract/test
+
+# Run test.
+cd $PATH_TO_SUBMISSION_REPO/contract
+npm install
+npx hardhat test
