2962f54eea4b614a28af6ff0f0351b078cfccf9c
Yuka Saito <yksaito02@gmail.com>
2023-01-05 17:21:06 +0900

    delete submodule and building a dfx environment

--- scripts/icp_basic_dex.sh
+++ scripts/icp_basic_dex.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Install dependencies.
+# The import path is from start.sh
+source utils/install_dfx.sh
+install_dfx
+
+
 # Clean existing test code.
 rm -rf $PATH_TO_SUBMISSION_REPO/scripts/test.sh
 
-# Copy original test code
+# Copy original test code.
 cp -r $PATH_TO_TEST_SOURCE_REPO/scripts/test.sh $PATH_TO_SUBMISSION_REPO/scripts/test.sh
 
 # Run test.
 cd $PATH_TO_SUBMISSION_REPO
-git submodule update --init --recursive
+
+## Execute delete command reference to submodule.
+rm -rf ./src/DIP20
+git clone https://github.com/Psychedelic/DIP20.git ./src/DIP20
 
 dfx stop
 rm -rf .dfx
