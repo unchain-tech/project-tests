1be754a0d9e2ffd996bebdb69fec5cc613fd9d2f
ryojiroakiyama <ryojiro25@gmail.com>
2023-01-07 13:33:56 +0900

    comment

--- scripts/near_bike_sharing.sh
+++ scripts/near_bike_sharing.sh
@@ -9,11 +9,12 @@ install_rust
 SUBMISSION_LIB=$PATH_TO_SUBMISSION_REPO/contract/src/lib.rs
 TEST_LIB=$PATH_TO_TEST_SOURCE_REPO/contract/src/lib.rs
 
-# Clean existing test code in lib.rs.
+# Clean existing test code in submission file.
 # Remove lines after "cfg(test)".
 sed -i "" '/cfg(test)/,$d' $SUBMISSION_LIB
 
 # Extract test code.
+# Remove lines before "cfg(test)".
 sed -n -i "" '/cfg(test)/,$p' $TEST_LIB
 
 # Append test code to submission file.
