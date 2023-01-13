0f37a54d413dabe16c75dafa0a143106746cb086
ryojiroakiyama <ryojiro25@gmail.com>
2023-01-13 17:39:31 +0900

    separate files

--- start.sh
+++ start.sh
@@ -6,34 +6,8 @@
 OPTIONS=eux
 set -$OPTIONS
 
-clone_repo()
-{
-    URL=$1
-    ACCESS_TOKEN=$2
-    REPO_NAME=$3
-
-    SCHEME=https://
-    DOMAIN=${URL##$SCHEME}
-    USER=x-access-token
-    git clone $SCHEME$USER:$ACCESS_TOKEN@$DOMAIN $REPO_NAME
-}
-
-run_test()
-{
-    SCRIPT=$1
-    SUBMISSION_REPO_NAME=$2
-    TEST_SOURCE_REPO_NAME=$3
-
-    export PATH_TO_SUBMISSION_REPO=$PWD/$SUBMISSION_REPO_NAME
-    export PATH_TO_TEST_SOURCE_REPO=$PWD/$TEST_SOURCE_REPO_NAME
-
-    chmod 755 $SCRIPT
-    bash -$OPTIONS $SCRIPT
-}
-
 #****************     SETTINGS     *******************************************************
 
-# List test resources corresponding to PROJECT_ID.
 # PROJECT_ID of star-pass (555~557) is randomly selected from not in use.
 case "$PROJECT_ID" in
     "402" )
@@ -89,10 +63,11 @@ fi
 SUBMISSION_REPO_NAME=submission
 TEST_SOURCE_REPO_NAME=test_source
 
+source utils/clone_repo.sh
 clone_repo $SUBMISSION_REPO_URL $SUBMITTER_PAT $SUBMISSION_REPO_NAME
-
 clone_repo $TEST_SOURCE_REPO_URL $TEST_SOURCE_ACCESS_TOKEN $TEST_SOURCE_REPO_NAME
 
+source utils/run_test.sh
 run_test scripts/$SCRIPT $SUBMISSION_REPO_NAME $TEST_SOURCE_REPO_NAME
 
 #****************     END TEST     *******************************************************
