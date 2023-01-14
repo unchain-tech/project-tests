f457c0bc25c96994f3a38a37cfb11e492f137efa
ryojiroakiyama <ryojiro25@gmail.com>
2023-01-14 17:12:11 +0900

    rm access token

--- start.sh
+++ start.sh
@@ -12,52 +12,42 @@ set -$OPTIONS
 case "$PROJECT_ID" in
     "402" )
         TEST_SOURCE_REPO_URL=https://github.com/unchain-dev/near-hotel-booking-dapp.git
-        TEST_SOURCE_ACCESS_TOKEN=
         SCRIPT=near_hotel_booking.sh
         ;;
     "403" )
         TEST_SOURCE_REPO_URL=https://github.com/unchain-dev/NEAR-Bike-Sharing.git
-        TEST_SOURCE_ACCESS_TOKEN=
         SCRIPT=near_bike_sharing.sh
         ;;
     "501" )
         TEST_SOURCE_REPO_URL=https://github.com/unchain-dev/AVAX-Messenger.git
-        TEST_SOURCE_ACCESS_TOKEN=
         SCRIPT=avax_messenger.sh
         ;;
     "502" )
         TEST_SOURCE_REPO_URL=https://github.com/unchain-dev/AVAX-Amm.git
-        TEST_SOURCE_ACCESS_TOKEN=
         SCRIPT=avax_amm.sh
         ;;
     "503" )
         TEST_SOURCE_REPO_URL=https://github.com/unchain-dev/AVAX-Asset-Tokenization.git
-        TEST_SOURCE_ACCESS_TOKEN=
         SCRIPT=avax_asset_tokenization.sh
         ;;
     "504" )
         TEST_SOURCE_REPO_URL=https://github.com/unchain-dev/AVAX-Subnet.git
-        TEST_SOURCE_ACCESS_TOKEN=
         SCRIPT=avax_subnet.sh
         ;;
     "555" )
         TEST_SOURCE_REPO_URL=https://github.com/shiftbase-inc/STARPASS-test-code.git
-        TEST_SOURCE_ACCESS_TOKEN=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
         SCRIPT=starpass_soc.sh
         ;;
     "556" )
         TEST_SOURCE_REPO_URL=https://github.com/shiftbase-inc/STARPASS-test-code.git
-        TEST_SOURCE_ACCESS_TOKEN=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
         SCRIPT=starpass_gov.sh
         ;;
     "557" )
         TEST_SOURCE_REPO_URL=https://github.com/shiftbase-inc/STARPASS-test-code.git
-        TEST_SOURCE_ACCESS_TOKEN=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
         SCRIPT=starpass_med.sh
         ;;
     "602" )
         TEST_SOURCE_REPO_URL=https://github.com/unchain-dev/icp_basic_dex.git
-        TEST_SOURCE_ACCESS_TOKEN=
         SCRIPT=icp_basic_dex.sh
         ;;
     * ) echo "No match PROJECT_ID"
@@ -67,7 +57,8 @@ esac
 
 if [ -z $TEST_SOURCE_ACCESS_TOKEN ]; then
     # If the environment variable is not set, a dummy is set.
-    TEST_SOURCE_ACCESS_TOKEN=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
+    DUMMY_TOKEN=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
+    TEST_SOURCE_ACCESS_TOKEN=$DUMMY_TOKEN
 fi
 
 #****************     END SETTINGS     ****************************************************
