7cc15e3705d7476e14597de67c30ab2ee3aa1a7b
github honganji <yujicavs@icloud.com>
2023-02-21 10:04:26 +0900

    the problem was fixed and confirm the test runs correctly

--- scripts/ganache-yield-farm.sh
+++ scripts/ganache-yield-farm.sh
@@ -9,7 +9,15 @@ cp -r $PATH_TO_TEST_SOURCE_REPO/test $PATH_TO_SUBMISSION_REPO/test
 # Run test.
 cd $PATH_TO_SUBMISSION_REPO
 npm install
-echo truffle test | truffle(develop)> 
-truffle develop 
+
+expect -c "
+ spawn truffle develop
+ expect truffle(develop)>
+ send \"truffle test\n\"
+
+ expect truffle(develop)>
+ send \".exit\n\"
+ interact
+"
 
 
