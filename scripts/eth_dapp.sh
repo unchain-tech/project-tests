9a904c7649c7b51e2fb882f669aee7cddc0ad066
github honganji <yujicavs@icloud.com>
2023-03-13 10:29:23 +0900

    fix test script path

--- scripts/eth_dapp.sh
+++ scripts/eth_dapp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Clean existing test code.
-rm -rf $PATH_TO_SUBMISSION_REPO/packages/contract/scripts/run.js
+rm -rf $PATH_TO_SUBMISSION_REPO/packages/contract/test/test.js
 
 # Copy original test code
-cp -r $PATH_TO_TEST_SOURCE_REPO/packages/contract/scripts/run.js $PATH_TO_SUBMISSION_REPO/packages/contract/scripts/run.js
+cp -r $PATH_TO_TEST_SOURCE_REPO/packages/contract/test/test.js $PATH_TO_SUBMISSION_REPO/packages/contract/test/test.js
 
 # Run test.
 cd $PATH_TO_SUBMISSION_REPO
