e08260bbac3d942f3310bb45a178bbc37a06bd4b
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-04-18 15:11:06 +0900

    feat: update path and command

--- scripts/avax_amm.sh
+++ scripts/avax_amm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Clean existing test code.
-rm -rf $PATH_TO_SUBMISSION_REPO/contract/test
+rm -rf $PATH_TO_SUBMISSION_REPO/packages/contract/test/AMM.ts
 
 # Copy original test code
-cp -r $PATH_TO_TEST_SOURCE_REPO/contract/test $PATH_TO_SUBMISSION_REPO/contract/test
+cp -r $PATH_TO_TEST_SOURCE_REPO/packages/contract/test/AMM.ts $PATH_TO_SUBMISSION_REPO/packages/contract/test/AMM.ts
 
 # Run test.
-cd $PATH_TO_SUBMISSION_REPO/contract
+cd $PATH_TO_SUBMISSION_REPO
 npm install
-npx hardhat test
+npm run test
