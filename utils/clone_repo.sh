9bba091afd739a3a6390e54dd085ba96795987ea
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-04-25 11:53:57 +0900

    refactor: modify comment; refactor: uniform format;

--- utils/clone_repo.sh
+++ utils/clone_repo.sh
@@ -7,17 +7,13 @@ clone_submission_repo()
     REPO_NAME=$3
     IS_DEV=$4
 
-    echo $IS_DEV
-
     SCHEME=https://
     DOMAIN=${URL##$SCHEME}
     USER=x-access-token
-    if [[ ("$IS_DEV" == "yes") && ("$IS_STARTER_PRESENT" = "yes")]]
-    then
-      echo "Starter project is present."
+    if [[ ("$IS_DEV" == "yes") && ("$IS_STARTER_PRESENT" == "yes")]]; then
+      echo "Clone from complete branch for testing."
       git clone -b complete $SCHEME$USER:$ACCESS_TOKEN@$DOMAIN $REPO_NAME
     else
-      echo "Starter project is not present."
       git clone $SCHEME$USER:$ACCESS_TOKEN@$DOMAIN $REPO_NAME
     fi
 }
@@ -32,7 +28,7 @@ clone_test_source_repo()
     SCHEME=https://
     DOMAIN=${URL##$SCHEME}
     USER=x-access-token
-    if [ "$IS_STARTER_PRESENT" = "yes" ]; then
+    if [[ "$IS_STARTER_PRESENT" == "yes" ]]; then
       echo "Starter project is present."
       git clone -b complete $SCHEME$USER:$ACCESS_TOKEN@$DOMAIN $REPO_NAME
     else
