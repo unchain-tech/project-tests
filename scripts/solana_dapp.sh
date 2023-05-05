de1c2871a91a8122fa5921c69b44b10dfc76bd90
github honganji <yujicavs@icloud.com>
2023-05-05 17:37:41 +0900

    delete unused code

--- scripts/solana_dapp.sh
+++ scripts/solana_dapp.sh
@@ -14,34 +14,24 @@ yarn
 # コントラクトのプロジェクトに移動
 cd packages/contract
 
-# 2. ローカルネットワークをバックエンドで起動させる
-solana-test-validator -r --quiet &
-
-# 立ち上げまで5秒間待つ
-sleep 5
+# 2. ローカルネットワークをバックエンドで起動させる + 立ち上げまで5秒間待つ
+solana-test-validator -r --quiet & sleep 5
 
 # 3. 新しいウォレットの作成と必要なSOLの取得
 solana-keygen new -o fooKey.json --force --no-bip39-passphrase
 solana config set --keypair fooKey.json && solana config set --url localhost
 solana-keygen new -o target/deploy/myepicproject-keypair.json --force --no-bip39-passphrase
 
-# 4. コントラクトのbuild, deploy, test
-yarn build
-v=$(solana-keygen pubkey target/deploy/myepicproject-keypair.json)
-sed -i '' -e "3s/^.*/declare_id!(\"$v\");/" ./programs/myepicproject/src/lib.rs
-sed -i '' -e "6s/^.*/myepicproject = \"$v\"/" Anchor.toml
-
-yarn build
-# Run test.
-anchor run test
-
-
-sed -i "" "13s/^.*/wallet = \"fooKey.json\"/" Anchor.toml
+# 4. 必要なトークンの付与
 solana airdrop 6
+
+# 5. コントラクトのbuild, deploy, test
 yarn build
 yarn deploy
 v=`solana address -k target/deploy/myepicproject-keypair.json`
-sed -i "" "3s/^.*/declare_id!(\"$v\");/" programs/myepicproject/src/lib.rs && sed -i "" "6s/^.*/myepicproject = \"$v\"/" Anchor.toml
+sed -i '' -e "3s/^.*/declare_id!(\"$v\");/" ./programs/myepicproject/src/lib.rs
+sed -i '' -e "6s/^.*/myepicproject = \"$v\"/" Anchor.toml
+
 yarn build
 yarn deploy
 yarn test
