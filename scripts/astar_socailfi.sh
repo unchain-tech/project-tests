65d9d6751810b03db308c6a40736e31e0114b385
github honganji <yujicavs@icloud.com>
2023-05-05 16:55:47 +0900

    use nightly version for cargo

--- scripts/astar_socailfi.sh
+++ scripts/astar_socailfi.sh
@@ -4,6 +4,9 @@
 # The import path is from start.sh
 source utils/install_rust.sh
 install_rust
+rustup update    
+rustup override set nightly
+rustup update nightly
 
 # File name.
 SUBMISSION_LIB=$PATH_TO_SUBMISSION_REPO/packages/contract/lib.rs
@@ -11,11 +14,11 @@ TEST_LIB=$PATH_TO_TEST_SOURCE_REPO/packages/contract/lib.rs
 
 # Clean existing test code in submission file.
 # Remove lines after "cfg(test)".
-sed -i '/cfg(test)/,$d' $SUBMISSION_LIB
+sed -i "" '/cfg(test)/,$d' $SUBMISSION_LIB
 
 # Extract test code.
 # Remove lines before "cfg(test)".
-sed -n -i '/cfg(test)/,$p' $TEST_LIB
+sed -n -i "" '/cfg(test)/,$p' $TEST_LIB
 
 # Append test code to submission file.
 cat $TEST_LIB >> $SUBMISSION_LIB
