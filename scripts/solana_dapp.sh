11ca2cd590cf9102fbd8b3050704ddeba30c79a5
github honganji <yujicavs@icloud.com>
2023-05-19 08:52:26 +0900

    add path for openssl

--- scripts/solana_dapp.sh
+++ scripts/solana_dapp.sh
@@ -5,6 +5,8 @@
 
 # Ubuntuの環境構築
 sudo apt-get update && sudo apt-get install -y pkg-config build-essential libudev-dev libssl-dev
+export LD_LIBRARY_PATH="/root/local/lib:$LD_LIBRARY_PATH"
+
 
 source utils/install_rust.sh
 install_rust
