d3f2a281c45fb98227ad1a7276f666709077fa8c
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-05-18 11:05:58 +0900

    feat: add script; feat: set 303

--- start.sh
+++ start.sh
@@ -59,6 +59,11 @@ case "$PROJECT_ID" in
         SCRIPT=solana_dapp.sh
         IS_STARTER_PRESENT="yes"
         ;;
+    "303" )
+        TEST_SOURCE_REPO_URL=https://github.com/unchain-tech/Solana-Online-Store.git
+        SCRIPT=solana_online_store.sh
+        IS_STARTER_PRESENT="yes"
+        ;;
     "304" )
         TEST_SOURCE_REPO_URL=https://github.com/unchain-tech/Solana-Wallet.git
         SCRIPT=solana_wallet.sh
