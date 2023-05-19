68c647f06a4313d7055588a4cc463d0e20a83f65
github honganji <yujicavs@icloud.com>
2023-05-19 13:01:06 +0900

    add path

--- scripts/solana_dapp.sh
+++ scripts/solana_dapp.sh
@@ -5,7 +5,8 @@
 
 # Ubuntuの環境構築
 sudo apt-get update && sudo apt-get install -y pkg-config build-essential libudev-dev libssl-dev
-find . -name 'libssl.so.1.1'
+export LD_LIBRARY_PATH="/root/local/lib:$LD_LIBRARY_PATH"
+
 
 source utils/install_rust.sh
 install_rust
