307af6780bac8b691267eb112bc5277390284a6a
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-08-16 17:55:25 +0900

    refactor: remove .env.local file setup

--- scripts/eth_dao.sh
+++ scripts/eth_dao.sh
@@ -6,11 +6,7 @@ rm -rf $PATH_TO_SUBMISSION_REPO/test/test.ts
 # Copy original test code
 cp -r $PATH_TO_TEST_SOURCE_REPO/test/test.ts $PATH_TO_SUBMISSION_REPO/test/test.ts
 
-# make .env.local file and add valueable
-cd $PATH_TO_SUBMISSION_REPO
-touch .env.local
-echo -e "PRIVATE_KEY=$PRIVATE_KEY\nWALLET_ADDRESS=$WALLET_ADDRESS\nALCHEMY_API_URL=$ALCHEMY_API_URL" > .env.local
-
 # Run test.
+cd $PATH_TO_SUBMISSION_REPO
 yarn install
 yarn test
