ca491dbe6324f9e1a6f6eedca908cea2e593ab11
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-09-27 19:12:12 +0900

    feat: add script

--- start.sh
+++ start.sh
@@ -54,6 +54,11 @@ case "$PROJECT_ID" in
         SCRIPT=polygon_mobile_dapp.sh
         IS_STARTER_PRESENT="no"
         ;;
+    "204" )
+        TEST_SOURCE_REPO_URL=https://github.com/unchain-tech/Polygon-Whitelist-NFT.git
+        SCRIPT=polygon_whitelist_nft.sh
+        IS_STARTER_PRESENT="no"
+        ;;
     "301" )
         TEST_SOURCE_REPO_URL=https://github.com/unchain-tech/Solana-dApp.git
         SCRIPT=solana_dapp.sh
